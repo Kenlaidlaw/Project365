e2058dc6129cbfbb78737a21e0c8485ae1e78fae
Ken Laidlaw <avalaf@yahoo.com>
2022-01-02 20:41:53 +0000

    Project365 D2Commit

--- timetrack.js
+++ timetrack.js
@@ -11,3 +11,16 @@ const hoursUsed = () => {
   sleepNum = parsInt(sleep.value);
   hoursAvailable.innerText = sleepNum;
 };
+
+// practice sets
+
+const answer = document.querySelector("#answer");
+
+function addInputs() {
+  const numOne = document.querySelector("#infoNum1").value;
+  const numTwo = document.querySelector("#infoNum2").value;
+
+  let sum = parseInt(numOne) + parseInt(numTwo);
+
+  answer.innerText = sum;
+}
